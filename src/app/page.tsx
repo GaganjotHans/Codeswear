398ea359292bfe903042f764b5ac790df5af0521
Gaganjot <gaganjothans777@gmail.com>
2024-02-29 20:25:46 -0500

    More components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <div>
         <img src="/home.webp" alt="" />
-       
       </div>
-      <Footer/>
     </div>
-  )
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div>
       <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center shadow-md w-full">
           <Link
             href={"/"}
             className="flex title-font font-medium items-center text-gray-900"
@@ -15,10 +15,18 @@ const Navbar = () => {
             <Image src="/logo.png" alt="logo" width={200} height={40} />
           </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-gray-900">Tshirts</a>
-            <a className="mr-5 hover:text-gray-900">Hoodies</a>
-            <a className="mr-5 hover:text-gray-900">Stickers</a>
-            <a className="mr-5 hover:text-gray-900">Mugs</a>
+            <Link href={"/tshirts"} className="mr-5 hover:text-gray-900">
+              Tshirts
+            </Link>
+            <Link href={"/hoodies"} className="mr-5 hover:text-gray-900">
+              Hoodies
+            </Link>
+            <Link href={"/stickers"} className="mr-5 hover:text-gray-900">
+              Stickers
+            </Link>
+            <Link href={"/mugs"} className="mr-5 hover:text-gray-900">
+              Mugs
+            </Link>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Login
